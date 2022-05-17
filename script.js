9e6c6a5687d26d804cabd2267a84c9a2255b864e
lawal azeez <olatunjibabatundejunior@gmail.com>
2022-05-17 14:51:37 -0700

    password and email optimization

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 "use strict";
-
+const emailType =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 let userName = document.getElementById("name");
 let userEmail = document.getElementById("email");
 let userPassword = document.getElementById("password");
 let userNewPassword = document.getElementById("newpassword");
 let userPhone = document.getElementById("phone");
 const toggle = document.querySelector(".toggle");
-
+const newToggle = document.querySelector(".newToggle");
 const submit = document.querySelector(".btn");
 
 submit.addEventListener("click", function() {
     if (userName.value === "") {
         userName.placeholder = " kindly input your username";
         alert("kindly input your username");
-    } else if (userEmail.value === "") {
+        //        Email validation
+    } else if (userEmail.value === "" || !userEmail.value.match(emailType)) {
         userEmail.placeholder = " kindly input your email address";
         alert("invalid email address");
     } else if (userPassword.value === "") {
         userPassword.placeholder = "your password should be equal to new password";
         alert("your password please");
     } else if (
-        userNewPassword.value == "" &&
-        userNewPassword.value !== userPassword.value
+        //          Password validation
+        userNewPassword.value == "" ||
+        userPassword.value !== userNewPassword.value
     ) {
         userNewPassword.placeholder = "input password ";
         alert(" re-enter your password");
@@ -30,15 +33,23 @@ submit.addEventListener("click", function() {
         alert("enter your mobile number");
     } else {
         alert(
-            `Dear ${userName.value} your record is with us thanks for the registration`
+            `Dear ${userName.value} your record is with us , thanks for the registration to our bootcamp`
         );
     }
 });
 
+//                   togglers
 toggle.addEventListener("click", function() {
     if (userPassword.type === "password") {
         userPassword.type = "text";
     } else {
         userPassword.type = "password";
+    }
+});
+newToggle.addEventListener("click", function() {
+    if (userNewPassword.type === "password") {
+        userNewPassword.type = "text";
+    } else {
+        userNewPassword.type = "password";
     }
 });
